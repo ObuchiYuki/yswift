387b2b6e6e7c706182f9c54211431b42e6bf55c9
ObuchiYuki <yukibochi1@gmail.com>
2023-04-10 20:07:16 +0900

    add iOS support

--- Package.swift
+++ Package.swift
@@ -5,12 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "yswift",
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
         .library(name: "yswift", targets: ["yswift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ObuchiYuki/Promise.git", branch: "main"),
-        .package(url: "https://github.com/ObuchiYuki/lib0-swift.git", branch: "main"),
+        .package(url: "https://github.com/ObuchiYuki/Promise.git", .upToNextMajor(from: "1.0.14")),
+        .package(url: "https://github.com/ObuchiYuki/lib0-swift.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
